2ec266638ea0bdebcc7508bfb763fdddac3a4e29
DMancia03 <diego.mancia.03@gmail.com>
2024-08-06 21:48:18 -0600

    Creación de chatbot

---
+++ src/components/Chatbot.jsx
@@ -0,0 +1,54 @@
+'use client'
+
+import React, {useState, useId} from "react"
+import axios from "axios"
+import Message from "./Message"
+
+const Chatbot = () => {
+    //Mensaje del usuario
+    const [userMessage, setUserMessage] = useState({});
+
+    //Mensajes
+    const [messages, setMessages] = useState([]);
+
+    //Preguntar al bot
+    const handleSend = async () => {
+        if(userMessage.text != undefined && userMessage.text.trim()){
+            setMessages([...messages, userMessage]); //Agregar mensaje del usuario
+            
+            try{
+                const response = await axios.post('api', {message: userMessage.text});
+                const botMessage = { text: response.data.reply, sender: 'bot' };
+                setMessages([...messages, botMessage]); //Agregar mensaje del bot
+            }catch(er){
+                console.error(er); //Mostrar error en la consola
+                const errorMessage = { text: 'El bot parece no contestar. Ponte en contacto con soporte técnico.', sender: 'error' };
+                setMessages([...messages, errorMessage]); //Agregar mensaje de error
+            }
+        }
+
+        setUserMessage({ text: '', sender: 'user' }); //Limpiar mensaje del usuario
+    }
+
+    //Crear mensaje del usuario
+    const createUserMessage = (e) => {
+        setUserMessage({ text: e.target.value, sender: 'user' });
+    }
+
+    //Componente html
+    return (
+        <div>
+            <div>
+                {messages.map((value, ind) => (<Message msg={value} key={ind} />))}
+            </div>
+            <div>
+                <input type="text" name="userMessage" value={userMessage.text} onChange={(e) => createUserMessage(e)} />
+            </div>
+            <div>
+                <button onClick={handleSend}>Enviar</button>
+            </div>
+        </div>
+    )
+}
+
+export default Chatbot
---
+++ src/components/Message.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+const Message = ({msg}) => {
+    return (
+        <div>
+            <p>{msg.sender}: {msg.text}</p>
+        </div>
+    )
+}
+
+export default Message
